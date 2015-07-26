cbf12a7c6bb1bac26a2aa24106e1047194b29ba0
Crisskho <salemkho@gmail.com>
2015-07-27 00:05:06 +0800

    2nd commit to include comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,19 @@
 
+## Below two functions defined are to computes inverse matrix
+## of an input matrix.
+## They have to be used together as below:
+## cacheSolve(makeCacheMatrix(y)) where y is input matrix
+## It will either return either value of inverse_matrix.
+## or calculate from input matrix for 1st time. After that,
+## it will always capture from existing inverse_matrix.
 
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix simply a list of functions defined and
+## does not performance any calculation by itself
+## Four function have been created which is 
+## set, get, setsolve and getsolve
 
 makeCacheMatrix <- function(x = matrix()) {
-  ##inverse_matrix <- NULL
   
   set <- function(test_matrix) {
     x <<- test_matrix
@@ -23,17 +30,26 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve return an inverse matrix of input matrix.
+## It will either return either value of inverse_matrix.
+## or calculate from input matrix for 1st time. After that,
+## it will always capture from existing inverse_matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## inverse_matrix2 is local variable here
+  
   inverse_matrix2 <- x$getsolve()
+  
   if(!is.null(inverse_matrix2)) {
     message("getting cached data")
     return(inverse_matrix2)
   }
+
+## below only run when the inverse_matrix2 is NULL
   data <- x$get()
   inverse_matrix2 <- solve(data, ...)
+
+## this is to keep local value back to inverse_matrix
   x$setsolve(inverse_matrix2)
   
   inverse_matrix2
